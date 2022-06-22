ad049e2f2a80265bda4f2f3758c794925e918bc9
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-22 14:25:06 +0100

    Cleaned up code a little

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -1,7 +1,6 @@
 const pageContent = document.querySelector('#content')
 
 const createHeader = () => {
-
     // Creates the header element
     const header = document.createElement('header');
     header.classList.add('header');
@@ -46,6 +45,7 @@ const createFooter = () => {
     // Creates the footer element
     const footer = document.createElement('footer');
     footer.classList.add('footer');
+    
     // Creates the footer paragraph element
     const footerPara = document.createElement('p');
     footerPara.classList.add('footer-para');
