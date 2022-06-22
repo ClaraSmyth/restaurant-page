1c8352cafd369f458c855d6099c566eac8341f61
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-22 11:14:04 +0100

    Added more content to the header

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -11,20 +11,36 @@ const createHeader = () => {
     title.classList.add('header-title');
     title.innerText = 'Restaurant Page';
 
-    header.append(title);
-
     // Creates the nav element
     const nav = document.createElement('nav');
     nav.classList.add('header-nav');
 
+    //Creates the ul element within nav
     const navList = document.createElement('ul');
     navList.classList.add('header-nav-list');
+
+    //Creates the li elements within the ul
+    const home = createListItem('Home');
+    const menu = createListItem('Menu');
+    const contact = createListItem('Contact');
+
+    // Adds the nav li items to the ul
+    navList.append(home, menu, contact);
+    // Adds the nav ul to the nav element 
     nav.append(navList);
-
+    // Adds the title and nav to the header
+    header.append(title);
     header.append(nav);
-
     // Adds the header to the pageContent div
     pageContent.append(header);
+}
+
+const createListItem = (name) => {
+    //Creates the ul list item
+    const navListItem = document.createElement('li');
+    navListItem.classList.add('header-nav-list-item');
+    navListItem.innerText = name;
+    return navListItem;
 }
 
 export { createHeader };
