86d6bde53c0872d2d769527fb0dbf13a46ae4746
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-25 20:23:21 +0100

    Added additional content to home page

--- src/modules/home.js
+++ src/modules/home.js
@@ -5,10 +5,10 @@ const createTitle = () => {
     return title;
 }
 
-const createImg = () => {
+const createImg = (className, source) => {
     const img = document.createElement('img')
-    img.classList.add('main-img')
-    img.setAttribute('src', './assets/donut.png');
+    img.classList.add(className)
+    img.setAttribute('src', source);
     img.setAttribute('alt', '');
     return img;
 }
@@ -17,17 +17,22 @@ const createDesc = () => {
     const para = document.createElement('p');
     const lineBreak = document.createElement('br');
     para.classList.add('main-para');
-    para.append('We hand make our donuts everyday with fresh ingredients!', lineBreak, 'Check out our menu to see our standard range of donuts. Each day we also make a variety of unique specialty donuts that aren\'t shown on our menu so come visit our store to see what is available today!');
+    para.append('We hand make our donuts everyday with fresh ingredients!', lineBreak, 'Check out our menu to see our standard range of donuts. Each day we also make a variety of unique specialty donuts that aren\'t shown on our menu so come visit our store to see what\'s available today!');
     return para;
 }
 
 const createHome = () => {
     const main = document.querySelector('.main');
     main.textContent = '';
+    const mainContentContainer = document.createElement('div');
+    mainContentContainer.classList.add('main-content-container');
     const title = createTitle();
-    const img = createImg();
+    const img = createImg('main-img-one', './assets/donut.png');
+    const imgTwo = createImg('main-img-two', './assets/donut-2.png');
     const para = createDesc();
-    main.append(title, img, para);
+
+    mainContentContainer.append(para, img, imgTwo);
+    main.append(title, mainContentContainer);
 }
 
 export { createHome }
